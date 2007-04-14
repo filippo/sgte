fbc435562d8f9a880c1a51850ee11d1f87c6f861
Filippo Pacini <filippo.pacini@gmail.com>
2007-04-14 23:04:16 +0000

    test functions renaming

--- test/src/compile.erl
+++ test/src/compile.erl
@@ -1,51 +1,56 @@
--module(sgte_test_compile).
+-module(compile).
 
--export([test_compile_attr/0, test_compile_include/0, test_compile_apply/0]).
--export([test_compile_map/0, test_compile_mapl/0]).
--export([test_compile_mapj/0, test_compile_mmap/0]).
--export([test_compile_inline_map/0, test_compile_if/0]).
+-export([test_attr/0, test_attr_multiline/0]).
+-export([test_include/0, test_apply/0]).
+-export([test_map/0, test_mapl/0]).
+-export([test_mapj/0, test_mmap/0]).
+-export([test_inline_map/0, test_if/0]).
 
 %%--------------------
 %%
 %% Tests
 %%
 %%--------------------
-%%
-%% Compile Test
-%%
-test_compile_attr() ->
+test_attr() ->
     Str = "foo $bar$ baz",
     {ok, Compiled} = sgte:compile(Str),
     sgeunit:assert_equal(Compiled, "foo " ++ [{attribute, bar, 1}] ++ " baz").
 
-test_compile_include() ->
+test_attr_multiline() ->
+    Str = "foo "
+	"$bar$ "
+	"baz",
+    {ok, Compiled} = sgte:compile(Str),
+    sgeunit:assert_equal(Compiled, "foo \n" ++ [{attribute, bar, 2}] ++ "\n baz").
+
+test_include() ->
     {ok, C} = sgte:compile("foo $include tmpl$ baz"),
     sgeunit:assert_equal(C, "foo " ++ [{include, tmpl, 1}] ++ " baz").
 
-test_compile_apply() ->
+test_apply() ->
     {ok, C} = sgte:compile("foo $apply bar myVar$ baz"),
     sgeunit:assert_equal(C, "foo " ++ [{apply, {bar, myVar}, 1}] ++ " baz").
 
-test_compile_map() ->
+test_map() ->
     {ok, C} = sgte:compile("foo $map bar varList$ baz"),
     sgeunit:assert_equal(C, "foo " ++ [{map, {bar, varList}, 1}] ++ " baz").
 
-test_compile_mmap() ->
+test_mmap() ->
     {ok, C} = sgte:compile("foo $mmap bar baz varList$"),
     sgeunit:assert_equal(C, "foo " ++ [{mmap, {[bar, baz], varList}, 1}]).
 
-test_compile_mapl() ->
+test_mapl() ->
     {ok, C} = sgte:compile("foo $mapl bar varList$"),
     sgeunit:assert_equal(C, "foo " ++ [{mapl, {bar, varList}, 1}]).
 
-test_compile_mapj() ->
+test_mapj() ->
     {ok, C} = sgte:compile("foo $mapj bar varList separator$"),
     sgeunit:assert_equal(C, "foo " ++ [{mapj, {bar, varList, separator}, 1}]).
 
-test_compile_inline_map() ->
+test_inline_map() ->
     sgeunit:fail().
 
-test_compile_if() ->
+test_if() ->
     sgeunit:fail().
 %%     {ok, C} = sgte:compile(simple_if()),
 %%     Result = "Start " ++ [{ift, {{attribute, test}, "then branch", "else branch"}}],
