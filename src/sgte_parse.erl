f5fb518303c5a99527478dc0ea90d0c2c6793753
Filippo Pacini <filippo.pacini@gmail.com>
2007-04-18 17:03:59 +0000

    updates for line number management

--- src/sgte_parse.erl
+++ src/sgte_parse.erl
@@ -44,8 +44,8 @@ parse("$include"++T, Parsed, Line) ->
     P = and_parser([fun strip_blank/1, 
 		    until(fun is_dollar/1)]),
     case P(T) of
-	{ok, [Token], Rest} ->
-	    parse(Rest, [{include, Token, Line}|Parsed], Line);
+	{ok, [Token], LinesParsed, Rest} ->
+	    parse(Rest, [{include, Token, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {include, Reason, Line}}
     end;
@@ -54,8 +54,8 @@ parse("$apply"++T, Parsed, Line) ->
 		    until_space(fun is_blank/1), 
 		    until(fun is_dollar/1)]),
     case P(T) of
-	{ok, [F, V], Rest} ->
-	    parse(Rest, [{apply, {F, V}, Line}|Parsed], Line);
+	{ok, [F, V], LinesParsed, Rest} ->
+	    parse(Rest, [{apply, {F, V}, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {apply, Reason, Line}}
     end;
@@ -64,8 +64,8 @@ parse("$mapl"++T, Parsed, Line) ->
 		    until_space(fun is_blank/1), 
 		    until(fun is_dollar/1)]),    
     case P(T) of
-	{ok, [Tmpl, VList], Rest} ->
-	    parse(Rest, [{mapl, {Tmpl, VList}, Line}|Parsed], Line);
+	{ok, [Tmpl, VList], LinesParsed, Rest} ->
+	    parse(Rest, [{mapl, {Tmpl, VList}, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {mapl, Reason, Line}}
     end;
@@ -75,8 +75,8 @@ parse("$mapj"++T, Parsed, Line) ->
 		    until_space(fun is_blank/1), 
 		    until(fun is_dollar/1)]),    
     case P(T) of
-	{ok, [Tmpl, VList, Join], Rest} ->
-	    parse(Rest, [{mapj, {Tmpl, VList, Join}, Line}|Parsed], Line);
+	{ok, [Tmpl, VList, Join], LinesParsed,Rest} ->
+	    parse(Rest, [{mapj, {Tmpl, VList, Join}, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {mapj, Reason, Line}}
     end;
@@ -85,8 +85,8 @@ parse("$mmap"++T, Parsed, Line) ->
 		    until_greedy(fun is_blank/1), 
 		    until(fun is_dollar/1)]),    
     case P(T) of
-	{ok, [TmplL, VList], Rest} ->
-	    parse(Rest, [{mmap, {TmplL, VList}, Line}|Parsed], Line);
+	{ok, [TmplL, VList], LinesParsed, Rest} ->
+	    parse(Rest, [{mmap, {TmplL, VList}, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {mmap, Reason, Line}}
     end;
@@ -95,12 +95,12 @@ parse("$map:"++T, Parsed, Line) ->
 	     until(fun is_dollar/1)],
     P = and_parser(Rules),    
     case P(T) of
- 	{ok, [Inline, VList], Rest} ->
+ 	{ok, [Inline, VList], LinesParsed, Rest} ->
  	    case parse(Inline) of
  		{error, {Tok, Reason, L}} ->
  		    {error, {Tok, Reason, Line+L}};
  		{ok, InlP} ->
- 		    parse(Rest, [{imap, {[InlP], VList}, Line}|Parsed], Line)
+ 		    parse(Rest, [{imap, {[InlP], VList}, Line}|Parsed], Line+LinesParsed)
  	    end;
  	{error, Reason} -> 
  	    {error, {imap, Reason, Line}}
@@ -110,8 +110,8 @@ parse("$map"++T, Parsed, Line) ->
 		    until_space(fun is_blank/1), 
 		    until(fun is_dollar/1)]),    
     case P(T) of
-	{ok, [Tmpl, VList], Rest} ->
-	    parse(Rest, [{map, {Tmpl, VList}, Line}|Parsed], Line);
+	{ok, [Tmpl, VList], LinesParsed, Rest} ->
+	    parse(Rest, [{map, {Tmpl, VList}, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {map, Reason, Line}}
     end;
@@ -121,18 +121,18 @@ parse("$join"++T, Parsed, Line) ->
 	     until(fun is_dollar/1)],
     P = and_parser(Rules),    
     case P(T) of
- 	{ok, [Separator, VList], Rest} ->
-	    parse(Rest, [{join, {Separator, VList}, Line}|Parsed], Line);
+ 	{ok, [Separator, VList], LinesParsed, Rest} ->
+	    parse(Rest, [{join, {Separator, VList}, Line}|Parsed], Line+LinesParsed);
  	{error, Reason} -> 
  	    {error, {imap, Reason, Line}}
     end;
 parse("$if "++T, Parsed, Line) ->
     %% if uses the code from the old version. See if it can be improved
-    IfTmpl = collect_ift(T, [], {}),
+    IfTmpl = collect_ift(T),
     case IfTmpl of
 	{error, Reason} ->
 	    {error, {ift, Reason, Line}};
-	{ift, IfToken, Rest1} ->
+	{ift, IfToken, _LinesParsed, Rest1} ->
 	    case parse_ift(IfToken) of
 		{error, Reason} -> {error, {ift, Reason, Line}};
 		{ift, ParsedIf} -> 
@@ -144,8 +144,8 @@ parse([H|T], Parsed, Line) when H == $$ andalso hd(T) == $$ ->
 parse([H|T], Parsed, Line) when H == $$ ->
     P =  until(fun is_dollar/1),
     case P(T) of
-	{ok, Token, Rest} ->
-	    parse(Rest, [{attribute, Token, Line}|Parsed], Line);
+	{ok, Token, LinesParsed, Rest} ->
+	    parse(Rest, [{attribute, Token, Line}|Parsed], Line+LinesParsed);
 	{error, Reason} -> 
 	    {error, {attribute, Reason, Line}}
     end;
@@ -156,7 +156,7 @@ parse([H|T], Parsed, Line) when [H] == "\r" andalso hd(T) == "\n" ->
 parse([H|T], Parsed, Line) when [H] == "\r" orelse [H] == "\n" ->
     parse(T, [H|Parsed], Line+1);
 parse([H|T], Parsed, Line) ->
-    {ok, H1, T1} = simple([H|T]),
+    {ok, H1, _L, T1} = simple([H|T]),
     parse(T1, [H1|Parsed], Line).
 
    
@@ -164,36 +164,41 @@ parse([H|T], Parsed, Line) ->
 %% Internal functions
 %%====================================================================     
 %% collect if token till $end if$
-collect_ift([], _Token, _T) ->
-    {error, end_not_found};
-collect_ift("\\$"++Rest, Token, T) -> %% Escape sequence for \, $, {, }
-    collect_ift(Rest, ["$"|Token], T);
-collect_ift("\\\\"++Rest, Token, T) -> %% Escape sequence for \, $, {, }
-    collect_ift(Rest, ["\\"|Token], T);
-collect_ift("$end if$"++Rest, Token, {Test, Then}) ->
-    {ift, {Test, Then, lists:reverse(Token)}, Rest};
-collect_ift("$end if$"++Rest, Token, {Test}) ->
-    {ift, {Test, lists:reverse(Token)}, Rest};
-collect_ift("$else$"++Rest, Token, {Test}) ->
-    collect_ift(Rest, [], {Test, lists:reverse(Token)});
+collect_ift(Tmpl) ->
+    collect_ift(Tmpl, [], {}, 0).
 
-%% Nested if
-collect_ift("$if "++Rest, Token, {Test}) ->
-    case collect_ift(Rest, [], {}) of
-	{ift, InnerIf, Rest1} ->
+collect_ift([], _Token, _T, _Line) ->
+    {error, end_not_found};
+collect_ift("\\$"++Rest, Token, T, Line) -> %% Escape sequence for \, $, {, }
+    collect_ift(Rest, ["$"|Token], T, Line);
+collect_ift("\\\\"++Rest, Token, T, Line) -> %% Escape sequence for \, $, {, }
+    collect_ift(Rest, ["\\"|Token], T, Line);
+collect_ift("$end if$"++Rest, Token, {Test, Then}, Line) ->
+    {ift, {Test, Then, lists:reverse(Token)}, Line, Rest};
+collect_ift("$end if$"++Rest, Token, {Test}, Line) ->
+    {ift, {Test, lists:reverse(Token)}, Line, Rest};
+collect_ift("$else$"++Rest, Token, {Test}, Line) ->
+    collect_ift(Rest, [], {Test, lists:reverse(Token)}, Line);
+collect_ift("$if "++Rest, Token, {Test}, Line) ->  %% Nested if
+    case collect_ift(Rest, [], {}, Line) of
+	{ift, InnerIf, LinesParsed, Rest1} ->
 	    case parse_ift(InnerIf) of
 		{error, Reason} -> 
 		    {error, Reason};
 		ParsedIf -> 
-		    collect_ift(Rest1,[ParsedIf, lists:reverse(Token)], {Test})
+		    collect_ift(Rest1,[ParsedIf, lists:reverse(Token)], {Test}, Line+LinesParsed)
 	    end;
 	{error, E} -> 
-	    collect_ift(Rest, [{error, E}, lists:reverse(Token)], {Test})
+	    {error, E}
     end;
-collect_ift([H|Rest], Token, {}) when [H] == "$" ->
-    collect_ift(Rest, [], {lists:reverse(Token)});
-collect_ift([H|Rest], Token, T) ->
-    collect_ift(Rest, [H|Token], T).
+collect_ift([H|Rest], Token, {}, Line) when [H] == "$" ->
+    collect_ift(Rest, [], {lists:reverse(Token)}, Line);
+collect_ift([H|Rest], Token, T, Line) when [H] == "\r" andalso hd(T) == "\n" ->
+    collect_ift(Rest, ["\r\n"|Token], T, Line+1);
+collect_ift([H|Rest], Token, T, Line) when [H] == "\r" orelse [H] == "\n" ->
+    collect_ift(Rest, [H|Token], T, Line+1);
+collect_ift([H|Rest], Token, T, Line) ->
+    collect_ift(Rest, [H|Token], T, Line).
 
 
 %% if token parser
@@ -217,18 +222,18 @@ parse_ift({Test, Then}) ->
 %% And parser of Rules
 and_parser(Rules) ->
     fun(Tmpl) ->
-	    and_parser(Rules, Tmpl, [])
+	    and_parser(Rules, Tmpl, [], 0)
     end.
-and_parser([], Tmpl, SoFar) ->
-    {ok, lists:reverse(SoFar), Tmpl};
-and_parser([Rule|T], Tmpl, SoFar) ->
+and_parser([], Tmpl, SoFar, Line) ->
+    {ok, lists:reverse(SoFar), Line, Tmpl};
+and_parser([Rule|T], Tmpl, SoFar, Line) ->
     case Rule(Tmpl) of
 	{error, Reason} ->
 	    {error, Reason};
-	{ok, Rest} ->
-	    and_parser(T, Rest, SoFar);
-	{ok, Tok, Rest} ->
-	    and_parser(T, Rest, [Tok|SoFar])
+	{ok, Rest, LinesParsed} ->
+	    and_parser(T, Rest, SoFar, Line+LinesParsed);
+	{ok, Tok, LinesParsed, Rest} ->
+	    and_parser(T, Rest, [Tok|SoFar], Line+LinesParsed)
     end.
 
 %% %% Or parser of Rules
@@ -251,103 +256,119 @@ and_parser([Rule|T], Tmpl, SoFar) ->
 %%
 % simple: output whatever it gets in input
 simple([H|T]) ->
-    {ok, H, T}.
+    {ok, H, 0, T}.
 
 % strip blanks from Tmpl.
 strip_blank([H|T]) ->
     case is_blank(H) of
 	true ->
-	    strip_blank1(T);
+	    strip_blank1(T, 0);
 	_ ->
 	    {error, blank_not_found}
     end.
-strip_blank1([]) ->
-    {ok, []};
-strip_blank1([H|T]) ->
+strip_blank1([], Line) ->
+    {ok, [], Line};
+strip_blank1([H|T], Line) when [H]=="\r" andalso hd(T)=="\n" ->
+    strip_blank1(tl(T), Line+1);
+strip_blank1([H|T], Line) when [H]=="\r" orelse [H]=="\n" ->
+    strip_blank1(T, Line+1);
+strip_blank1([H|T], Line) ->
     case is_blank(H) of
 	true ->
-	    strip_blank1(T);
+	    strip_blank1(T, Line);
 	_ ->
-	    {ok, [H|T]}
+	    {ok, [H|T], Line}
     end.
 
 % until predicate P: output what it gets until P(H) is true stripping white spaces.
 until(P) ->
-    fun (Tmpl) -> until(P, Tmpl, []) end.
-until(_P, [], _Parsed) ->    
+    fun (Tmpl) -> until(P, Tmpl, 0, []) end.
+until(_P, [], _Line, _Parsed) ->    
     {error, end_not_found};
-until(P, [H|T], Parsed) when [H]==" " orelse [H]=="\n" orelse[H]== "\r" ->
-    until(P, T, Parsed);
-until(P, [H|T], Parsed) ->
+until(P, [H|T], Line, Parsed) when [H]==" " ->
+    until(P, T, Line, Parsed);
+until(P, [H|T], Line, Parsed) when [H]=="\r" andalso hd(T)=="\n" ->
+    until(P, tl(T), Line+1, Parsed);
+until(P, [H|T], Line, Parsed) when [H]=="\n" orelse [H]== "\r" ->
+    until(P, T, Line+1, Parsed);
+until(P, [H|T], Line, Parsed) ->
     case P(H) of
 	true ->
-	    {ok, list_to_atom(lists:reverse(Parsed)), T};
+	    {ok, list_to_atom(lists:reverse(Parsed)), Line, T};
 	_ ->
-	    until(P, T, [H|Parsed])
+	    until(P, T, Line, [H|Parsed])
     end.
 
 % until predicate P without stripping white spaces
 until_space(P) ->
-    fun (Tmpl) -> until_space(P, Tmpl, []) end.
-until_space(_P, [], _Parsed) ->    
+    fun (Tmpl) -> until_space(P, Tmpl, 0, []) end.
+until_space(_P, [], _Line, _Parsed) ->
     {error, end_not_found};
-until_space(P, [H|T], Parsed) ->
+until_space(P, [H|T], Line, Parsed) when [H]=="\r" andalso hd(T)=="\n" ->
+    until_space(P, tl(T), Line+1, ["\r\n"|Parsed]);
+until_space(P, [H|T], Line, Parsed) when [H]=="\n" orelse [H]== "\r" ->
+    until_space(P, T, Line+1, [H|Parsed]);
+until_space(P, [H|T], Line, Parsed) ->
     case P(H) of
 	true ->
-	    {ok, list_to_atom(lists:reverse(Parsed)), T};
+	    {ok, list_to_atom(lists:reverse(Parsed)), Line, T};
 	_ ->
-	    until_space(P, T, [H|Parsed])
+	    until_space(P, T, Line, [H|Parsed])
     end.
 
 
 % Greedy version: output what it gets until a $ is reached.
 % If all template string ends returns an error
 until_greedy(P) ->
-    fun (Tmpl) -> until_greedy(P, Tmpl, [], []) end.
-until_greedy(_P, [], _StrSofFar, _ResList) ->    
+    fun (Tmpl) -> until_greedy(P, Tmpl, [], 0, []) end.
+until_greedy(_P, [], _StrSofFar, _Line, _ResList) ->    
     {error, end_not_found};
-until_greedy(_P, [H|T], StrSoFar, ResList) when [H] == "$" ->
+until_greedy(_P, [H|T], StrSoFar, Line, ResList) when [H] == "$" ->
     Rest = lists:reverse(StrSoFar) ++ "$",
-    {ok, lists:reverse(ResList), lists:flatten([Rest|T])};
-until_greedy(P, [H|T], StrSoFar, ResList) ->
+    {ok, lists:reverse(ResList), Line, lists:flatten([Rest|T])};
+until_greedy(P, [H|T], StrSoFar, Line, ResList) when [H]=="\r" andalso hd(T)=="\n" ->
+    until_greedy(P, T, StrSoFar, Line+1, ResList);
+until_greedy(P, [H|T], StrSoFar, Line, ResList) when [H]=="\n" orelse [H]== "\r" ->
+    until_greedy(P, T, StrSoFar, Line+1, ResList);
+until_greedy(P, [H|T], StrSoFar, Line, ResList) ->
     case P(H) of
 	true ->
 	    H1 = list_to_atom(string:strip(lists:reverse(StrSoFar))),
-	    until_greedy(P, T, "", [H1|ResList]);
+	    until_greedy(P, T, "", Line, [H1|ResList]);
 	false ->
-	    until_greedy(P, T, [H|StrSoFar], ResList)
+	    until_greedy(P, T, [H|StrSoFar], Line, ResList)
     end.
 
 % Match parenthesis: Start and Stop are two predicates which matches
 % open and closed parenthesis. Inner parenthesis are coleccted to be parsed later.
 parenthesis(Start, Stop) ->
-    fun (Tmpl) -> parenthesis(Start, Stop, Tmpl, 0, []) end.
+    fun (Tmpl) -> parenthesis(Start, Stop, Tmpl, 0, [], 0) end.
 
-parenthesis(_Start, _Stop, [], _Count, _StrSoFar) ->
+parenthesis(_Start, _Stop, [], _Count, _StrSoFar, _Line) ->
     {error, end_not_found};
-parenthesis(Start, Stop, [H|T], Count, StrSoFar) when Count == 0 ->
+parenthesis(Start, Stop, [H|T], Count, StrSoFar, Line) when Count == 0 ->
     case Start(H) of
 	true ->
-	    parenthesis(Start, Stop, T, Count+1, StrSoFar);
+	    parenthesis(Start, Stop, T, Count+1, StrSoFar, Line);
 	_ ->
 	    {error, start_not_found}
     end;
-parenthesis(Start, Stop, [H|T], Count, StrSoFar) when Count > 0 ->
+parenthesis(Start, Stop, [H|T], Count, StrSoFar, Line) when Count > 0 ->
     case Stop(H) of
 	true ->
 	    Count1 = Count - 1,
 	    case Count1 == 0 of
 		true ->
-		    {ok, lists:reverse(StrSoFar), T};
+		    {ok, lists:reverse(StrSoFar), Line, T};
 		_ ->
-		    parenthesis(Start, Stop, T, Count1, [H|StrSoFar])
+		    parenthesis(Start, Stop, T, Count1, [H|StrSoFar], Line)
 	    end;
 	_ ->
 	    case Start(H) of
 		true ->
-		    parenthesis(Start, Stop, T, Count+1, [H|StrSoFar]);
+		    parenthesis(Start, Stop, T, Count+1, [H|StrSoFar], Line);
 		_ ->
-		    parenthesis(Start, Stop, T, Count, [H|StrSoFar])
+		    parenthesis(Start, Stop, T, Count, [H|StrSoFar], Line)
 	    end
     end.
 
