e28e5ff4b1d0099557f5fd63a8b26327ab788825
Filippo Pacini <filippo.pacini@gmail.com>
2007-05-19 16:41:52 +0000

    improved test for the quiet option

--- test/src/sgte_test_quiet.erl
+++ test/src/sgte_test_quiet.erl
@@ -1,9 +1,9 @@
 -module(sgte_test_quiet).
 
 -export([test_string/0, test_include/0, test_apply/0]).
--export([test_simpleif/0, test_simpleif_no_test/0]).
+-export([test_simpleif/0]).
 -export([test_fif/0, test_fif2/0, test_nested_fif/0, test_if/0]).
--export([test_fun/0, test_file/0]).
+-export([test_fun/0]).
 
 
 %%--------------------
@@ -28,91 +28,67 @@ test_string() ->
      sgeunit:assert_equal(Res2, ResultStr2)].
 
 test_include() ->
-    {ok, C1} = sgte:compile("bar"),
-    {ok, C2} = sgte:compile("foo $include tmpl$ baz"),
-    Res1 = sgte:render(C2, [], [quiet]),
-    Res2 = sgte:render(C2, []),
+    {ok, C1} = sgte:compile("foo $include tmpl$ baz"),
+    Res1 = sgte:render(C1, [], [quiet]),
+    Res2 = sgte:render(C1, []),
     ResultStr1 = "foo  baz",
     ResultStr2 = "foo [SGTE Warning: template: include - key tmpl not found on line 1] baz",
     [sgeunit:assert_equal(Res1, ResultStr1),
      sgeunit:assert_equal(Res2, ResultStr2)].
 
 test_apply() ->
-    F = fun(L) -> lists:nth(2, L) end,
     {ok, C} = sgte:compile("foo $apply second myList$ baz"),
-    Res = sgte:render(C, [{second, F}, {myList, ["1", "2", "3"]}]),
-    ResultStr = "foo 2 baz",
+    Res = sgte:render(C, [], [quiet]),
+    ResultStr = "foo  baz",
     sgeunit:assert_equal(Res, ResultStr).
 
 test_simpleif() ->
     {ok, C} = sgte:compile(simple_if()),
-    DThen = [{test, true}],
-    DElse = [{test, false}],
+    DThen = [],
+    DElse = [],
     RThen = sgte:render(C, DThen),
-    RElse = sgte:render(C, DElse),
-    [sgeunit:assert_equal(RThen, "Start then branch"),
+    RElse = sgte:render(C, DElse, [quiet]),
+    [sgeunit:assert_equal(RThen, "Start [SGTE Warning: template: ift - key test not found on line 1]"),
      sgeunit:assert_equal(RElse, "Start else branch")].
-
-test_simpleif_no_test() ->
-    {ok, C} = sgte:compile(simple_if()),
-    RElse = sgte:render(C, []),
-    sgeunit:assert_equal(RElse, "Start [SGTE Warning: template: ift - key test not found on line 1]").
 
 test_if() ->
     {ok, Compiled} = sgte:compile(if_string()),
-    NameL = mountainList(),
-    Data1 = [{testNames, true},
-	    {nameList, NameL}],
-    Data2 = [{testNames, false},
-	    {noName, fun no_name/1}],
-    Res1 = sgte:render(Compiled, Data1),
-    Res2 = sgte:render(Compiled, Data2),
-    [sgeunit:assert_equal(Res1, "Hello! Some Mountains: Monte Bianco, Cerro Torre, Mt. Everest, Catinaccio Bye Bye."),
-     sgeunit:assert_equal(Res2, "Hello! No Name Found Bye Bye.")].    
+    Data1 = [{testNames, true}],
+    Data2 = [],
+    Res1 = sgte:render(Compiled, Data1, [quiet]),
+    Res2 = sgte:render(Compiled, Data2, [quiet]),
+    [sgeunit:assert_equal(Res1, "Hello! Some Mountains:  Bye Bye."),
+     sgeunit:assert_equal(Res2, "Hello!  Bye Bye.")].    
     
 test_fif() ->
     {ok, Compiled} = sgte:compile(if_string()),
     NameL = mountainList(),
-    Data = [{testNames, check_names(NameL)},
-	    {noName, fun no_name/1},
-	    {nameList, NameL}],
-    Res = sgte:render(Compiled, Data),
-    sgeunit:assert_equal(Res, "Hello! Some Mountains: Monte Bianco, Cerro Torre, Mt. Everest, Catinaccio Bye Bye.").
+    Data = [{testNames, check_names(NameL)}],
+    Res = sgte:render(Compiled, Data, [quiet]),
+    sgeunit:assert_equal(Res, "Hello! Some Mountains:  Bye Bye.").
 
 test_fif2() ->
     {ok, Compiled} = sgte:compile(if_string()),
     D1 = dict:new(),
     D2 = dict:store('testNames', check_names([]), D1),
-    D3 = dict:store('noName', fun no_name/1, D2),
-    D4 = dict:store('nameList', mountainList(), D3),
-    Res = sgte:render(Compiled, D4),
-    sgeunit:assert_equal(Res, "Hello! No Name Found Bye Bye.").
+    Res = sgte:render(Compiled, D2, [quiet]),
+    sgeunit:assert_equal(Res, "Hello!  Bye Bye.").
 
 test_nested_fif() ->
     {ok, Compiled} = sgte:compile(nested_if_string()),
     NameL = mountainList(),
     D1 = dict:new(),
     D2 = dict:store('testNames', check_names(NameL), D1),
-    D3 = dict:store('noName', fun no_name/1, D2),
-    D4 = dict:store('nameList', NameL, D3),
-    Res = sgte:render(Compiled, D4),
-    sgeunit:assert_equal(Res, "Some Mountains: Monte Bianco, Cerro Torre, Mt. Everest, Catinaccio").
+    Res = sgte:render(Compiled, D2, [quiet]),
+    Res2 = sgte:render(Compiled, [], [quiet]),
+    [sgeunit:assert_equal(Res, "Some Mountains: "),
+     sgeunit:assert_equal(Res2, "")].
 
 % test callable attribute
 test_fun() ->
-    MyF = fun(Data) ->
-		  {ok, V} = dict:find(foo, Data),
-		  "TEST: " ++ V
-	  end,
     {ok, CF} = sgte:compile(tmpl_fun()),
-    Res = sgte:render(CF, [{foo, "foooo"}, {callme, MyF}]),
-    sgeunit:assert_equal(Res, "aaaa TEST: foooo bbb").
-
-%test on a non existent file
-test_file() ->
-    Res = sgte:compile_file("myfile.tmpl"),
-    sgeunit:assert_equal(Res, {error, enoent}).
-
+    Res = sgte:render(CF, [{foo, "foooo"}], [quiet]),
+    sgeunit:assert_equal(Res, "aaaa  bbb").
 
 %%--------------------
 %%
@@ -138,9 +114,6 @@ nested_if_string() ->
 	"Some Mountains: $if testNames$$join:{, } nameList$$end if$" ++
         "$else$" ++
 	"$noName$$end if$".
-
-no_name(_Foo) ->
-    "No Name Found".
 
 check_names(NameList) ->
     length(NameList) > 0.
